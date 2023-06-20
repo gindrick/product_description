a3c96c99502579c73d3e83f7d685662db923653a
Jay2Jay <gindrick@gmail.com>
2023-06-20 08:42:31 +0200

    Upravena verze z Open AI API dema na generovani popisu produktu

--- app.py
+++ app.py
@@ -4,17 +4,22 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        name = request.form["animal"]
+        params = request.form["params"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=generate_prompt(name, params),
+            temperature=0.6,            
+            max_tokens=500,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -22,14 +27,8 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_prompt(name, params):
+    return """Napiš mi prosím popis produktu na eshop aspoň na 300 slov pro produkt s názvem: {} a s parametry: {}""".format(
+        name.capitalize(), params
     )
+
